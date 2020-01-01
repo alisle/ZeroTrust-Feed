c497f5fc489558dbd0ec7f06ae4d7bb9f4de2216
Alex Lisle <alex.lisle@gmail.com>
2020-01-01 14:21:32 -0600

    Actually start making this useful

--- build.py
+++ build.py
@@ -2,8 +2,14 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 use_plugin("python.install_dependencies")
+use_plugin("python.flake8")
+use_plugin("python.coverage")
+use_plugin("python.distutils")
+use_plugin("pypi:pybuilder_docker")
 
+name="zerotrust-feed"
 default_task="publish"
+
 
 @init
 def initialize(project):
